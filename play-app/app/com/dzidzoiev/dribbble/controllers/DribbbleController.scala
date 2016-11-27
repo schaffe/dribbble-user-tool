68c5281e8ef55fec55524d71d922b889c3a1610d
dzidzoiev <artur.dzidzoiev@themidnightcoders.com>
2016-11-27 16:09:00 +0200

    actors added to handling multi-page requests

--- play-app/app/com/dzidzoiev/dribbble/controllers/DribbbleController.scala
+++ play-app/app/com/dzidzoiev/dribbble/controllers/DribbbleController.scala
@@ -2,12 +2,17 @@ package com.dzidzoiev.dribbble.controllers
 
 import javax.inject.Inject
 
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.contrib.pattern.Aggregator
 import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json.{JsValue, Json}
 import play.api.libs.ws.WSClient
+import play.api.libs.ws.ahc.AhcCurlRequestLogger
 import play.api.mvc.{Action, Controller}
+import play.libs.akka.AkkaGuiceSupport
 
-import scala.concurrent.Future
+import scala.collection.mutable.{ArrayBuffer, ListBuffer}
+import scala.concurrent.{Future, Promise}
 
 class DribbbleController @Inject()(dribbleService: DribbleService) extends Controller {
   implicit val userWrites = Json.writes[User]
@@ -16,13 +21,57 @@ class DribbbleController @Inject()(dribbleService: DribbleService) extends Contr
   def getFollowers(userId: String) = Action.async {
     dribbleService.getFollowers(userId)
       .map(followers => Ok(Json.toJson(followers)))
-//      .recover(case e: DribbleException => InternalServerError(e))
+    //      .recover(case e: DribbleException => InternalServerError(e))
   }
 }
 
-class DribbleService @Inject()(client: DribbleRestClient) {
+class UserActor @Inject()(client: DribbleRestClient) extends Actor with Aggregator {
 
-  def getFollowers(id: String) = client.getFollowers(id)
+
+  val pagesize = 10
+  val values = ListBuffer.empty[User]
+
+  expectOnce {
+    case id: String => fetchPage(id, 0)
+
+  }
+
+  expect {
+    case GetPage(id, page) => fetchPage(id, page)
+  }
+
+  def fetchPage(id: String, page: Int): Unit = {
+    client.getFollowers(id, page, pagesize).onSuccess {
+      case users if users.isEmpty =>
+        context.parent ! values.toList
+        context.stop(self)
+      case users =>
+        values ++= users
+        self ! GetPage(id, page + 1)
+    }
+  }
+
+  case class GetPage(id: String, page: Int)
+
+}
+
+class DribbleService @Inject()(system: ActorSystem) {
+
+  def getFollowers(id: String) = {
+
+    val userActor = system.actorOf(Props(classOf[UserActor]))
+
+    val p = Promise[List[User]]
+    val replyTo = system.actorOf(Props(new Actor {
+      def receive = {
+        case reply: List[User] =>
+          p.success(reply)
+          context.stop(self)
+      }
+    }))
+    userActor.tell(id, sender = replyTo)
+    p.future
+  }
 
 }
 
@@ -33,16 +82,21 @@ class DribbleRestClient @Inject()(ws: WSClient) {
   val baseUrl = "https://api.dribbble.com/v1"
   val authKey = "b461e4de7c623018749a55b7e6eb5e8fe7e0ba0b6950764cb4c2552bff013e3c"
 
-  def getFollowers(id: String): Future[List[Follower]] = {
+  def getFollowers(id: String, page: Int, pagesize: Int): Future[List[User]] = {
     ws.url(baseUrl + "/users/" + id + "/followers")
+      .withRequestFilter(AhcCurlRequestLogger())
+      .withQueryString(("per_page", pagesize))
+      .withQueryString(("page", page))
       .withHeaders(("Authorization", "Bearer " + authKey))
       .get()
       .map({
         case resp if resp.status == 200 => resp.json
         case error => throw DribbleException(error.json)
       })
-      .map(userJson => userJson.validate[List[Follower]].get)
+      .map(userJson => userJson.validate[List[Follower]].get.map(f => f.follower))
   }
+
+  implicit def intToString(int: Int): String = int.toString
 }
 
 case class DribbleException(message: JsValue) extends RuntimeException(message.toString())
--- play-app/build.sbt
+++ play-app/build.sbt
@@ -7,3 +7,4 @@ lazy val `dribbble_user_tool` = (project in file(".")).enablePlugins(PlayScala)
 scalaVersion := "2.11.8"
 
 libraryDependencies ++= Seq( ws )
+libraryDependencies += "com.typesafe.akka" % "akka-contrib_2.11" % "2.4.10"
